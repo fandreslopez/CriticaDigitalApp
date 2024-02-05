9174688d7547733ef5d4cb00494906965bc040b3
fandreslopez <fandresm@live.com>
2024-02-04 21:35:24 -0300

    Proyecto inicializado en SpringInitializr

---
+++ src/main/java/grupo5/criticadigital/CriticaDigitalAppApplication.java
@@ -0,0 +1,13 @@
+package grupo5.criticadigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriticaDigitalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriticaDigitalAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo5/criticadigital/CriticaDigitalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo5.criticadigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriticaDigitalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
