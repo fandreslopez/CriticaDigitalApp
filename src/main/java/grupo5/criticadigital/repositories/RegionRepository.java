75de3409b174cf976fa282dccd50eea781e3f1da
Caatabs <caatabs@hotmail.com>
2024-02-07 23:12:00 -0300

    Added Region repository

---
+++ src/main/java/grupo5/criticadigital/repositories/RegionRepository.java
@@ -0,0 +1,7 @@
+package grupo5.criticadigital.repositories;
+
+import grupo5.criticadigital.models.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+}
