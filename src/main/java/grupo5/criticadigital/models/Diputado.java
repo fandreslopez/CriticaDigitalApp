4e1924d2d8bd481b9b76ca1eb7975579e0728e80
Caatabs <caatabs@hotmail.com>
2024-02-07 23:06:15 -0300

    Added the entity Diputado

---
+++ src/main/java/grupo5/criticadigital/models/Diputado.java
@@ -0,0 +1,29 @@
+package grupo5.criticadigital.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Diputado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idDiputado;
+
+    private String Nombres;
+
+    private String Apellidos;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date fechaDeNacimiento;
+
+    private Integer alias;
+
+    @ManyToOne
+    @JoinColumn(name = "regionId")
+    private Region idRegion;
+}
