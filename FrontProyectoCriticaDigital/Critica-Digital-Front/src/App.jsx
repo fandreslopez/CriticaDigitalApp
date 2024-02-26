158a2062f57bff6550738af5782394a5b1addc96
JimmyR99 <jr.jimmyrojas.99@gmail.com>
2024-02-26 16:08:49 -0300

    Front

---
+++ FrontProyectoCriticaDigital/Critica-Digital-Front/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./assets/components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div className="container">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontProyectoCriticaDigital/Critica-Digital-Front/src/assets/components/DatosJson/diputados.js
@@ -0,0 +1,257 @@
+let diputados = [
+  {
+    idDiputado: 74,
+    nombres: "Jaime",
+    apellidos: "Naranjo Ortiz",
+    fechaDeNacimiento: "1951-01-12T04:00:00.000+00:00",
+    alias: "PS",
+    idRegion: {
+      regionID: 18,
+      nombreRegion: "VII",
+    },
+  },
+  {
+    idDiputado: 803,
+    nombres: "René",
+    apellidos: "Alinco Bustos",
+    fechaDeNacimiento: "1958-06-02T04:00:00.000+00:00",
+    alias: "IND",
+    idRegion: {
+      regionID: 27,
+      nombreRegion: "XI",
+    },
+  },
+  {
+    idDiputado: 815,
+    nombres: "Sergio",
+    apellidos: "Bobadilla Muñoz",
+    fechaDeNacimiento: "1958-03-25T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 20,
+      nombreRegion: "VIII",
+    },
+  },
+  {
+    idDiputado: 872,
+    nombres: "Jaime",
+    apellidos: "Mulet Martínez",
+    fechaDeNacimiento: "1963-08-03T04:00:00.000+00:00",
+    alias: "PCFRVS",
+    idRegion: {
+      regionID: 4,
+      nombreRegion: "III",
+    },
+  },
+  {
+    idDiputado: 917,
+    nombres: "Gastón",
+    apellidos: "Von Mühlenbrock Zamora",
+    fechaDeNacimiento: "1954-12-26T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 24,
+      nombreRegion: "XIV",
+    },
+  },
+  {
+    idDiputado: 948,
+    nombres: "Gaspar",
+    apellidos: "Rivas Sánchez",
+    fechaDeNacimiento: "1978-05-17T04:00:00.000+00:00",
+    alias: "PDGI",
+    idRegion: {
+      regionID: 6,
+      nombreRegion: "V",
+    },
+  },
+  {
+    idDiputado: 953,
+    nombres: "Frank",
+    apellidos: "Sauerbaum Muñoz",
+    fechaDeNacimiento: "1972-11-21T03:00:00.000+00:00",
+    alias: "RN",
+    idRegion: {
+      regionID: 19,
+      nombreRegion: "XVI",
+    },
+  },
+  {
+    idDiputado: 971,
+    nombres: "Bernardo",
+    apellidos: "Berger Fett",
+    fechaDeNacimiento: "1946-10-04T04:00:00.000+00:00",
+    alias: "RN",
+    idRegion: {
+      regionID: 24,
+      nombreRegion: "XIV",
+    },
+  },
+  {
+    idDiputado: 973,
+    nombres: "Karol",
+    apellidos: "Cariola Oliva",
+    fechaDeNacimiento: "1987-04-01T03:00:00.000+00:00",
+    alias: "PCFRVS",
+    idRegion: {
+      regionID: 9,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 975,
+    nombres: "Daniella",
+    apellidos: "Cicardini Milla",
+    fechaDeNacimiento: "1987-12-31T03:00:00.000+00:00",
+    alias: "PS",
+    idRegion: {
+      regionID: 4,
+      nombreRegion: "III",
+    },
+  },
+  {
+    idDiputado: 976,
+    nombres: "Juan Antonio",
+    apellidos: "Coloma Álamos",
+    fechaDeNacimiento: "1980-03-14T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 14,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 989,
+    nombres: "Joaquín",
+    apellidos: "Lavín León",
+    fechaDeNacimiento: "1979-05-25T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 8,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 990,
+    nombres: "Daniel",
+    apellidos: "Melo Contreras",
+    fechaDeNacimiento: "1979-02-18T03:00:00.000+00:00",
+    alias: "PS",
+    idRegion: {
+      regionID: 13,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 991,
+    nombres: "Vlado",
+    apellidos: "Mirosevic Verdugo",
+    fechaDeNacimiento: "1987-05-23T04:00:00.000+00:00",
+    alias: "MRLDC",
+    idRegion: {
+      regionID: 1,
+      nombreRegion: "XV",
+    },
+  },
+  {
+    idDiputado: 999,
+    nombres: "Jorge",
+    apellidos: "Rathgeb Schifferli",
+    fechaDeNacimiento: "1967-12-28T04:00:00.000+00:00",
+    alias: "RN",
+    idRegion: {
+      regionID: 22,
+      nombreRegion: "IX",
+    },
+  },
+  {
+    idDiputado: 1002,
+    nombres: "Leonardo",
+    apellidos: "Soto Ferrada",
+    fechaDeNacimiento: "1966-05-15T04:00:00.000+00:00",
+    alias: "PS",
+    idRegion: {
+      regionID: 14,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 1003,
+    nombres: "Renzo",
+    apellidos: "Trisotti Martínez",
+    fechaDeNacimiento: "1976-03-15T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 2,
+      nombreRegion: "I",
+    },
+  },
+  {
+    idDiputado: 1009,
+    nombres: "Jorge",
+    apellidos: "Alessandri Vergara",
+    fechaDeNacimiento: "1979-06-08T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 10,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 1012,
+    nombres: "Boris",
+    apellidos: "Barrera Moreno",
+    fechaDeNacimiento: "1970-05-03T04:00:00.000+00:00",
+    alias: "PCFRVS",
+    idRegion: {
+      regionID: 9,
+      nombreRegion: "RM",
+    },
+  },
+  {
+    idDiputado: 1013,
+    nombres: "Alejandro",
+    apellidos: "Bernales Maldonado",
+    fechaDeNacimiento: "1979-02-02T03:00:00.000+00:00",
+    alias: "MRLDC",
+    idRegion: {
+      regionID: 26,
+      nombreRegion: "X",
+    },
+  },
+  {
+    idDiputado: 1015,
+    nombres: "Jorge",
+    apellidos: "Brito Hasbún",
+    fechaDeNacimiento: "1990-01-12T03:00:00.000+00:00",
+    alias: "FA",
+    idRegion: {
+      regionID: 7,
+      nombreRegion: "V",
+    },
+  },
+  {
+    idDiputado: 1016,
+    nombres: "Miguel Ángel",
+    apellidos: "Calisto Águila",
+    fechaDeNacimiento: "1985-07-19T04:00:00.000+00:00",
+    alias: "PDGI",
+    idRegion: {
+      regionID: 27,
+      nombreRegion: "XI",
+    },
+  },
+  {
+    idDiputado: 1017,
+    nombres: "Álvaro",
+    apellidos: "Carter Fernández",
+    fechaDeNacimiento: "1980-10-09T04:00:00.000+00:00",
+    alias: "UDI",
+    idRegion: {
+      regionID: 12,
+      nombreRegion: "RM",
+    },
+  },
+];
+
+export default diputados;
